03d3b8c9edfbfafced1772749c2437ae5139d0e5
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-04-18 22:03:21 +0530

    Update train.py

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
+import os
 def layers(model):
     model.add(Dense(units = 100,input_shape=(11,),activation='relu'))
     model.add(Dense(units = 4,activation='relu'))
