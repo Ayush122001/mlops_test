7d1873442641f7c9d8f622d27251464a2b9864ef
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-04-18 22:12:41 +0530

    Create train1.py

---
+++ train1.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import os
+def layers(model):
+    model.add(Dense(units = 100,input_shape=(11,),activation='relu'))
+    model.add(Dense(units = 50,activation='relu'))
+    model.add(Dense(units = 4,activation='relu'))
+    return model
+
+ds  = pd.read_csv("wine.csv")
+X = ds[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+       'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+       'pH', 'sulphates50, 'alcohol']]
+y = ds['quality'
+      ]
+y=pd.get_dummies(y,drop_first=True)
+#0=bad
+#1=good
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=20)
+model = Sequential()
+model = layers(model)
+model.add(Dense(units = 1,activation='sigmoid'))
+model.compile(loss='binary_crossentropy' ,optimizer=Adam(learning_rate=0.00001))
+model.fit(x_train,y_train, validation_data=( x_test,y_test), epochs=10, verbose=0)
+accuracy = model.evaluate(x_test, y_test, verbose=0)
+accuracy = accuracy*100
+model.save("final.h5")
+os.system("echo {} > a.txt".format(accuracy))
