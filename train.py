ba118c917ec26a375d8ad71b8d516b8e9c03cb94
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-04-18 21:30:33 +0530

    Update train.py

--- train.py
+++ train.py
@@ -13,12 +13,16 @@ X = ds[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
        'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
        'pH', 'sulphates', 'alcohol']]
 y = ds['quality']
+y=pd.get_dummies(y,drop_first=True)
+#0=bad
+#1=good
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=20)
 model = Sequential()
 model = layers(model)
 model.add(Dense(units = 1,activation='sigmoid'))
 model.compile(loss='binary_crossentropy' ,optimizer=Adam(learning_rate=0.00001))
-model.fit(X,y, validation_data=(X,y), epochs=10, verbose=0)
-accuracy = model.evaluate(X, y, verbose=0)
+model.fit(x_train,y_train, validation_data=( x_test,y_test), epochs=10, verbose=0)
+accuracy = model.evaluate(x_test, y_test, verbose=0)
 accuracy = accuracy*100
 model.save("final.h5")
 os.system("echo {} > a.txt".format(accuracy))	
