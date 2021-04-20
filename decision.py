3e5f4776db5454965cce6e587c01ea04652e1e4c
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-04-20 17:39:48 +0530

    Update decision.py

--- decision.py
+++ decision.py
@@ -2,6 +2,15 @@ import os
 ac = os.popen("cat a.txt")
 a = ac.read()
 a = a.rstrip()
-if float(a) < 80:
+if float(a) < 85:
   os.system("rm -rf *.h5")
   os.system("python3 train1.py")
+  
+if float(a) < 85:
+  os.system("rm -rf *.h5")
+  os.system("python3 train2.py")
+  
+  
+if float(a) < 85:
+  os.system("rm -rf *.h5")
+  os.system("python3 train3.py")
