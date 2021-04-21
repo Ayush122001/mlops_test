e296394b9ff7cec438d5565d8b8e67d356963922
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-04-21 11:49:36 +0530

    Update decision.py

--- decision.py
+++ decision.py
@@ -7,12 +7,20 @@ while float(accuracy) < 60:
   if float(a) < 60:
     os.system("rm -rf *.h5")
     os.system("python3 train1.py")
+    ac = os.popen("cat a.txt")
+    a = ac.read()
+    a = a.rstrip()
+    accuracy = a
     if float(accuracy) > 60:
       break
   
   if float(a) < 60:
     os.system("rm -rf *.h5")
     os.system("python3 train2.py")
+    ac = os.popen("cat a.txt")
+    a = ac.read()
+    a = a.rstrip()
+    accuracy = a
     if float(accuracy) > 60:
       break
   
@@ -20,18 +28,30 @@ while float(accuracy) < 60:
   if float(a) < 60:
     os.system("rm -rf *.h5")
     os.system("python3 train3.py")
+    ac = os.popen("cat a.txt")
+    a = ac.read()
+    a = a.rstrip()
+    accuracy = a
     if float(accuracy) > 60:
       break
   
   if float(a) < 60:
     os.system("rm -rf *.h5")
     os.system("python3 train4.py")
+    ac = os.popen("cat a.txt")
+    a = ac.read()
+    a = a.rstrip()
+    accuracy = a    
     if float(accuracy) > 60:
       break
   
   if float(a) < 60:
     os.system("rm -rf *.h5")
     os.system("python3 train5.py")
+    ac = os.popen("cat a.txt")
+    a = ac.read()
+    a = a.rstrip()
+    accuracy = a    
     if float(accuracy) > 60:
       break
 print("Final Accuracy = ", accuracy)
