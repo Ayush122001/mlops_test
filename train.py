f84c8ace31cf99da8718996c0d13081543cc66db
Ayush Ganatra <65668878+Ayush122001@users.noreply.github.com>
2021-09-09 00:18:28 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,29 +1,44 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
 import os
-def layers(model):
-    model.add(Dense(units = 15,input_shape=(11,),activation='relu'))
-    model.add(Dense(units = 4,activation='relu'))
-    return model
-
-ds  = pd.read_csv("wine.csv")
+ds = pd.read_csv('wine.csv')
+y = pd.get_dummies(ds['quality'],drop_first=True)
+y_final = y.values
 X = ds[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
        'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
        'pH', 'sulphates', 'alcohol']]
-y = ds['quality']
-y=pd.get_dummies(y,drop_first=True)
-#0=bad
-#1=good
-x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.1,random_state=20)
-model = Sequential()
-model = layers(model)
-model.add(Dense(units = 1,activation='sigmoid'))
-model.compile(loss='binary_crossentropy' ,optimizer=Adam(learning_rate=0.00001))
-model.fit(x_train,y_train, validation_data=( x_test,y_test), epochs=10, verbose=0)
-import numpy as np
+X_final = X.values
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+def model_fucn(hp):
+    hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+    model = Sequential()
+    model.add(Dense(units=hp_units,activation='relu',input_shape=(11,)))
+    model.add(Dense(units=hp_units,activation='relu'))
+    model.add(Dense(units=1,activation='sigmoid'))
+    model.compile(optimizer=Adam(learning_rate=hp_learning_rate),loss='binary_crossentropy',metrics=['accuracy'])
+    return model
+import keras_tuner as kt
+# model_fucn = from where model can be created
+# objective = what we want to optimized
+# max_epochs = for deciding initial models
+
+tuner = kt.Hyperband(
+    hypermodel = model_fucn,
+    objective = 'val_accuracy',
+    max_epochs = 20)
+X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size=0.33, random_state=42)
+from tensorflow.keras.callbacks import EarlyStopping
+# monitor = what to do
+# patient = no. of epoch with no improvement 
+stop_early = EarlyStopping(monitor='val_loss', patience=3)
+tuner.search(X_train, y_train, epochs=10, validation_split=0.2, callbacks=[stop_early])
+#tuner.search(X_train, y_train, epochs=50, validation_split=0.2)
+best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+model = tuner.hypermodel.build(best_hps)
+model.fit(X_train, y_train, epochs=50)
 yhat = (model.predict([x_test]))
 yhat = np.round(yhat)
 y_test = y_test.to_numpy()
@@ -37,4 +52,4 @@ while n < total:
 accuracy = (count*100)/total
 print(accuracy)
 model.save("final.h5")
-os.system("echo {} > a.txt".format(accuracy))	
+os.system("echo {} > a.txt".format(accuracy))
